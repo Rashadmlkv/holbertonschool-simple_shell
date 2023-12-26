369e631f028adec015472940b7b1eec5e129ed11
Farid Hashimzade <124304902+farid-hashimzada@users.noreply.github.com>
2023-12-26 15:00:26 +0400

    Update hcp.c

--- hcp.c
+++ hcp.c
@@ -32,7 +32,7 @@ int hcp(void)
 	while (r > 0)
 	{
 		r = read(fd2, buf, BUFFER_SIZE);
-		w = write(fd, buf, r);
+		w = write(fd, buf, w);
 		if (r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
