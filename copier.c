045df726fb34c60b912de1211f2361f01ecc8a20
Farid Hashimzade <124304902+farid-hashimzada@users.noreply.github.com>
2023-12-26 15:06:17 +0400

    Update copier.c

--- copier.c
+++ copier.c
@@ -29,18 +29,7 @@ int copier(void)
 	}
 	umask(0);
 	fd = open(file_to, O_TRUNC | O_CREAT | O_WRONLY, 0777);
-	while (r > 0)
-	{
-		r = read(fd2, buf, BUFFER_SIZE);
-		w = write(fd, buf, r);
-		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
-		if (fd == -1 || w == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
-	}
+	
 	c = close(fd);
 	if (c == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
