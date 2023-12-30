d22a1e35e4590ccb18da80691c7c85596ebf9ae5
Rashadmlkv <resadmlkv@gmail.com>
2023-12-30 08:44:50 -0800

    Last push

--- O_X.h
+++ O_X.h
@@ -3,20 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <string.h>
 
-extern char **environ;
-
-char *read_command(void);
-void parse_arguments(char *command, char **args);
-int execute_command(char *command);
-void search_and_execute(char *args[], char *command);
-int main(void);
-
+void checkcommand(void);
+int splitcommand(char *str, char *stri);
+int hcp(void);
+int creatprocs(char *arg[]);
+int exec(char* arg[]);
 #endif
