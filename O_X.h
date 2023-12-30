550550334c221071187ffea6c91756aca9ab82e9
Rashadmlkv <resadmlkv@gmail.com>
2023-12-30 08:06:41 -0800

    MR~BEAN

--- O_X.h
+++ O_X.h
@@ -1,4 +1,25 @@
-#ifndef O_X_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+
+extern char **environ;
+
+char *read_command(void);
+void parse_arguments(char *command, char **args);
+int execute_command(char *command);
+void search_and_execute(char *args[], char *command);
+int main(void);
+
+#endif#ifndef O_X_H
 #define O_X_H
 
 #include <stdio.h>
